155deb118c52644ce121d78865849b1296a03438
YoukeWJP <wjp@youkeonline.cn>
2017-03-26 19:54:52 +0800

    增加支付方式调试后的页面

--- page/admin/pay.js
+++ page/admin/pay.js
@@ -56,8 +56,20 @@ require(['YOUKE.Util', 'YOUKE.Comm', 'YOUKE.Service', 'YOUKE.Widget.Alert'], fun
         $(this).addClass('active').siblings('li').removeClass('active');
     })
     .on('click', '#payList .status', function() {
-        Alert.showConfirm('确认开启/关闭', '确认开启/关闭改支付方式么？', function () {
-            console.log('开启/关闭了');
+        var $this = $(this),
+            status = $this.closest('li').attr('data-status'),
+            methodid = $this.closest('li').attr('data-methodid');
+        // 1、开启  2、关闭
+        var msg = status == '1' ? '关闭' : '开启',
+            operation = status == '1' ? 'close' : 'open',
+            payMethod = $this.siblings('.type').text();
+        Alert.showConfirm('确认' + msg, '确认' + msg + '支付方式<span style="color:red;">' + payMethod + '</span>么？', function() {
+            switchPaymentMethod({
+                methodid: methodid,
+                operation: operation
+            }, function() {
+                $this.text(status == '1' ? '开启' : '关闭').closest('li').toggleClass('close').attr('data-status', status == '1' ? '0' : '1');
+            });
         });
     })
     //支付方式列表相关操作 --- END
@@ -65,10 +77,47 @@ require(['YOUKE.Util', 'YOUKE.Comm', 'YOUKE.Service', 'YOUKE.Widget.Alert'], fun
 
     //获取支付方式列表 --- BEGIN
     function getPayList() {
-
+        $http.post({
+            url : 'api/setting/payment_methods',
+            success : function(data){
+                if (data.code === $comm.HttpStatus.OK) {
+                    var result = [],
+                        item,
+                        tpl = '<li class="{#statusCls#}" data-methodid="{#methodid#}" data-status="{#status#}">'
+                            +   '<span class="type">{#method_name#}</span>'
+                            +   '<span class="status">{#statusName#}</span>'
+                            + '</li>';
+                    for (var i = 0; i < data.data.length; i++) {
+                        item = data.data[i];
+                        result.push($util.strReplace(tpl, {
+                            '{#statusCls#}': item.status == '1' ? '' : 'close',
+                            '{#status#}': item.status,// 1、开启  2、关闭
+                            '{#methodid#}': item.methodid,
+                            '{#method_name#}': item.method_name,
+                            '{#statusName#}': item.status == '1' ? '关闭' : '开启',
+                        }));
+                    }
+                    $('#payList').html(result.join(''));
+                }
+            }
+        });
     }
     //获取支付方式列表 --- END
 
+    //开启/关闭支付方式
+    function switchPaymentMethod(item, cb){
+        $http.post({
+            url: 'api/setting/payment_method_switching',
+            data: item,
+            success: function(data) {
+                if (data.code === $comm.HttpStatus.OK) {
+                    $util.isFunction(cb) && cb();
+                } else {
+                    Alert.showError(data.message || '操作失败');
+                }
+            }
+        });
+    }
     $core.Ready(function() {
         console.log('pay');
         if ($util.getCurrentRole() === $comm.Role.admin) {
